b465c7633774adf8d5d48e15d657c69f989418eb
Andreika <andrey.klimenko.carry@gmail.com>
2022-09-28 16:55:33 +0200

    Add delete handler

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -83,8 +83,9 @@ func main() {
 	)
 
 	router := mux.NewRouter()
+
 	// AK TODO pass args through app?
-	setupRouter(router, commandRunner, choosePidor.New(m, s, w, sc))
+	setupRouter(router, commandRunner, choosePidor.New(m, s, w, sc), m)
 
 	commandRunner.Start(ctx)
 
--- cmd/api/router.go
+++ cmd/api/router.go
@@ -11,14 +11,15 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/kapitan123/telegrofler/internal/command"
 	"github.com/kapitan123/telegrofler/internal/command/choosePidor"
+	"github.com/kapitan123/telegrofler/internal/messenger"
 )
 
 // AK TODO we just can't pass all stuff here, we still need an abstraction to group configuration
 // temp solution with direct handler function
-func setupRouter(r *mux.Router, runner *command.Runner, pdr *choosePidor.ChoosePidor) {
+func setupRouter(r *mux.Router, runner *command.Runner, pdr *choosePidor.ChoosePidor, msgr *messenger.Messenger) {
 	r.HandleFunc("/callback", messageHandler(runner)).Methods("POST")
 	r.HandleFunc("/chat/{chatid}/pidoroftheday", choosePidorHandler(pdr)).Methods("POST")
-	r.HandleFunc("/chat/{chatid}/{messageid}", deleteMessageHandler(pdr)).Methods("DELETE")
+	r.HandleFunc("/chat/{chatid}/{messageid}", deleteMessageHandler(msgr)).Methods("DELETE")
 }
 
 // AK TODO send messages to a dead message quee
@@ -46,9 +47,7 @@ func messageHandler(runner *command.Runner) http.HandlerFunc {
 
 func choosePidorHandler(pdr *choosePidor.ChoosePidor) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		chatarg := mux.Vars(r)["chatid"]
-
-		chatId, err := strconv.ParseInt(chatarg, 10, 64)
+		chatId, err := extractChatId(r)
 		if err != nil {
 			log.WithError(err).Error("Failed trying to parse chat id", err)
 			w.WriteHeader(http.StatusBadRequest)
@@ -64,21 +63,24 @@ func choosePidorHandler(pdr *choosePidor.ChoosePidor) http.HandlerFunc {
 	}
 }
 
-// AK TODO actual handler
-func deleteMessageHandler(pdr *choosePidor.ChoosePidor) http.HandlerFunc {
+func deleteMessageHandler(msgr *messenger.Messenger) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		chatarg := mux.Vars(r)["chatid"]
-
-		chatId, err := strconv.ParseInt(chatarg, 10, 64)
+		chatId, err := extractChatId(r)
 		if err != nil {
 			log.WithError(err).Error("Failed trying to parse chat id", err)
 			w.WriteHeader(http.StatusBadRequest)
 		}
 
-		err = pdr.ChoosePidor(r.Context(), chatId)
+		messageid, err := extractMessageId(r)
+		if err != nil {
+			log.WithError(err).Error("Failed trying to parse message id", err)
+			w.WriteHeader(http.StatusBadRequest)
+		}
+
+		err = msgr.Delete(chatId, messageid)
 
 		if err != nil {
-			log.Error("Failed trying to choose a pidor", err)
+			log.Error("Failed trying to delete a message", err)
 			w.WriteHeader(http.StatusInternalServerError)
 		}
 		w.WriteHeader(http.StatusOK)
@@ -88,4 +90,16 @@ func deleteMessageHandler(pdr *choosePidor.ChoosePidor) http.HandlerFunc {
 func logContent(update *tgbotapi.Update) {
 	ujs, _ := json.Marshal(update)
 	log.Info("Callback content:", string(ujs))
+}
+
+func extractChatId(r *http.Request) (int64, error) {
+	chatarg := mux.Vars(r)["chatid"]
+
+	return strconv.ParseInt(chatarg, 10, 64)
+}
+
+func extractMessageId(r *http.Request) (int, error) {
+	messagearg := mux.Vars(r)["messageid"]
+
+	return strconv.Atoi(messagearg)
 }
