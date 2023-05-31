32be0cd42ca85e6ebcc79d6882991ae5269b87de
Andreika <andrey.klimenko.carry@gmail.com>
2023-05-31 09:56:44 +0200

    Split posts to a separate file

--- service/bot/infra/posts_firestore_repo.go
+++ service/bot/infra/posts_firestore_repo.go
@@ -2,7 +2,6 @@ package infra
 
 import (
 	"context"
-	"net/url"
 	"time"
 
 	"cloud.google.com/go/firestore"
@@ -103,92 +102,4 @@ func (s *FirestorePostsRepository) GetByExternalSourceUrl(ctx context.Context, u
 	}
 
 	return postModels[0].toDomainModel(), true, nil
-}
-
-type (
-	PostModel struct {
-		Id                string          `firestore:"-"`
-		ExternalSourceUrl string          `firestore:"external_source_url"`
-		Reactions         []ReactionModel `firestore:"reactions"`
-		PostedOn          time.Time       `firestore:"posted_on"`
-		ChatId            int64           `firestore:"chat_id"`
-		Poster            UserRefModel    `firestore:"poster"`
-	}
-
-	ReactionModel struct {
-		ReactToMessageId int          `firestore:"react_to_message_id"`
-		Text             string       `firestore:"text"`
-		PostedOn         time.Time    `firestore:"posted_on"`
-		Reactor          UserRefModel `firestore:"reactor"`
-	}
-
-	UserRefModel struct {
-		DisplayName string `firestore:"user_name"`
-		Id          int64  `firestore:"user_id"`
-	}
-)
-
-func (pm *PostModel) SetId(id string) {
-	pm.Id = id
-}
-
-func (urm UserRefModel) toDomainModel() domain.UserRef {
-	return domain.UserRef{
-		DisplayName: urm.DisplayName,
-		Id:          urm.Id,
-	}
-}
-
-func (rm ReactionModel) toDomainModel() domain.Reaction {
-	return domain.Reaction{
-		Text:     rm.Text,
-		PostedOn: rm.PostedOn,
-		Reactor:  rm.Reactor.toDomainModel(),
-	}
-}
-
-func (pm PostModel) toDomainModel() domain.Post {
-	reactions := lo.Map(pm.Reactions, func(rm ReactionModel, _ int) domain.Reaction {
-		return rm.toDomainModel()
-	})
-
-	url, _ := url.Parse(pm.ExternalSourceUrl)
-	return domain.Post{
-		Id:                pm.Id,
-		ExternalSourceUrl: url,
-		Reactions:         reactions,
-		PostedOn:          pm.PostedOn,
-		ChatId:            pm.ChatId,
-		Poster:            pm.Poster.toDomainModel(),
-	}
-}
-
-func MapPostToModel(p domain.Post) PostModel {
-	reactionModels := lo.Map(p.Reactions, func(r domain.Reaction, _ int) ReactionModel {
-		return MapReactionToModel(r)
-	})
-
-	return PostModel{
-		Id:                p.Id,
-		ExternalSourceUrl: p.ExternalSourceUrl.String(),
-		Reactions:         reactionModels,
-		PostedOn:          p.PostedOn,
-		ChatId:            p.ChatId,
-		Poster:            MapUserRefToModel(p.Poster),
-	}
-}
-
-func MapUserRefToModel(ur domain.UserRef) UserRefModel {
-	return UserRefModel{
-		DisplayName: ur.DisplayName,
-		Id:          ur.Id,
-	}
-}
-
-func MapReactionToModel(r domain.Reaction) ReactionModel {
-	return ReactionModel{
-		Text:     r.Text,
-		PostedOn: r.PostedOn,
-		Reactor:  MapUserRefToModel(r.Reactor),
-	}
 }
