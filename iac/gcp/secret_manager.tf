16c636b8046320e0f2a23b11116389e5729f6d0f
Andreika <andrey.klimenko.carry@gmail.com>
2023-06-04 18:08:01 +0200

    Formatting on terraform

--- iac/gcp/secret_manager.tf
+++ iac/gcp/secret_manager.tf
@@ -1,7 +1,7 @@
 data "google_secret_manager_secret" "downloader_cookies" {
-  secret   = "downloader_cookies"
+  secret = "downloader_cookies"
 }
 
 data "google_secret_manager_secret" "bot_token" {
-  secret   = "telegram_bot_token"
+  secret = "telegram_bot_token"
 }
